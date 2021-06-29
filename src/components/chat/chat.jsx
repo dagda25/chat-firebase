14d914e5fcd95cb6a82c83fa836142be8775e9c3
dagda25 <dagda25@gmail.com>
2021-06-29 12:45:29 +0300

    styles

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,42 +1,38 @@
-import React, {useEffect, useRef} from "react";
-import Message from "../message/message";
-import {useCollectionData} from "react-firebase-hooks/firestore";
+import React, { useEffect, useRef } from 'react';
+import Message from '../message/message';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import './chat.css';
 
 const Chat = ({ firestore }) => {
-
   const chatList = useRef(null);
 
   const [messages, loading] = useCollectionData(
-      firestore.collection('messages').orderBy('date')
-  )
-  console.log(messages)
+    firestore.collection('messages').orderBy('date')
+  );
 
   useEffect(() => {
     chatList.current.scrollTop = chatList.current.scrollHeight;
   }, [messages]);
-  
 
   return (
-      <section className="chat">
-        <div className="chat-header">
-          <div className="chat-back" onClick={() => {}}></div>
-          <div className="chat-title">Чат</div>
-        </div>
-        <div className="chat-list" ref={chatList}>
-          { loading ?
-          <div className="chat-loader"></div> :
-          messages ?
-            messages.map((message) => {
-              return <Message message={message} key={message.date}/>;
-            }) :
-            <div>No messages</div>
-          }
-        </div>
-      </section>
+    <section className="chat">
+      <div className="chat-header">
+        <div className="chat-back" onClick={() => {}}></div>
+        <div className="chat-title">Чат</div>
+      </div>
+      <div className="chat-list" ref={chatList}>
+        {loading ? (
+          <div className="chat-loader"></div>
+        ) : messages ? (
+          messages.map((message) => {
+            return <Message message={message} key={message.date} />;
+          })
+        ) : (
+          <div>No messages</div>
+        )}
+      </div>
+    </section>
   );
 };
-
-
 
 export default Chat;
--- src/components/send-form/send-form.jsx
+++ src/components/send-form/send-form.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './send-form.css';
 
-
-const SendForm = ({firestore}) => {
-
+const SendForm = ({ firestore }) => {
   const [value, setValue] = useState('');
 
   const sendMessage = async (evt) => {
     evt.preventDefault();
     firestore.collection('messages').add({
-        text: value,
-        date: new Date()
+      text: value,
+      date: new Date(),
     });
     setValue('');
-  }
+  };
 
   return (
     <section className="send-panel">
-      <form action="/" encType="multipart/form-data" onSubmit={sendMessage} className="send-form">
-        <textarea 
-          name="message" 
+      <form
+        action="/"
+        encType="multipart/form-data"
+        onSubmit={sendMessage}
+        className="send-form"
+      >
+        <textarea
+          name="message"
           className="send-input"
           value={value}
-          onChange={e => setValue(e.target.value)}
+          onChange={(e) => setValue(e.target.value)}
         />
-        <button type="submit" className="send-button">Отправить</button>
+        <button type="submit" className="send-button">
+          Отправить
+        </button>
         <button type="submit" className="send-button-mobile"></button>
       </form>
     </section>
-
   );
 };
-
 
 export default SendForm;
